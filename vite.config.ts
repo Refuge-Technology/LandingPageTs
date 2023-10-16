d5291719c9ea985eba203d7741734ce6050244c6
Khalil Shanti <kshan016@fiu.edu>
2023-10-15 23:19:28 -0400

    changes again

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: `[name].[hash].mjs`,
-        chunkFileNames: `[name].[hash].mjs`,
-      },
+      outDir: 'dist'
     },
-  },
   plugins: [react()],
 })
